4358da581e7ba393182d87f03a74cfcff6c96415
warwick <chris@darnand.net>
2018-12-30 22:10:14 +0900

    Project renamed and readied

---
+++ README.md
@@ -0,0 +1,53 @@
+# pap_logger
+##A 'prêt-à-porter' logger with a sysout StreamHandler and optionally, a SysLogHandler and/or a TimedRotatingFileHandler
+### Usage
+
+```python
+pap = PaPLogger(app_name="your_application_name")
+```
+
+
+Logging level, syslog server address and log file filename can easily be changed at runtime.
+```python
+pap.level = logging.INFO
+# Setting a log file path will activate the TimedRotatingFileHandler
+pap.log_file = Path("/var/log/new_log_file_name")
+# A string works too
+pap.log_file = "/var/log/new_log_file_name"
+# Setting it to None will remove the TimedRotatingFileHandler
+pap.log_file = None
+# Setting a hostname will activate the SysLogHandler
+pap.syslog_host = "your_syslog_server"
+# Setting it to None will remove the SysLogHandler
+pap.syslog_host = None
+```
+The log file can be prefixed by the hostname for cases where the machine where logs are created is necessary.
+
+When activated, the hostname is also written in the logs.
+```python
+pap.logfile_with_hostname = True
+```
+The sysout StreamHandler format changes with the logging level; from simple to WARNING or lower, to verbose when higher.
+
+This can be overriden with the verbose value.
+```python
+pap.verbose = True
+```
+
+The SysLogHandler will only log records with level WARNING or above and always in a verbose format.
+
+The TimedRotatingFileHandler is rotating by default every day, and 15 days of logs are kept.
+
+These values can be changed when instantiating LsslLogger:
+```python
+pap = PaPLogger(app_name="your_application_name", when='D', backup_count=15)
+```
+
+More detailed usage examples are given in the function _pap_logger_example.
+
+Please refer to the source code.
+
+###Compatibility information
+This module will only run with Python 3.5 and ulterior due to its use of f-strings.
+
+It has only been tested under GNU/Linux.
--- __init__.py
+++ __init__.py
@@ -7,24 +7,40 @@ import logging
 from logging import config
 from logging.handlers import SysLogHandler, TimedRotatingFileHandler
 from pathlib import Path
-import socket
+from socket import gethostname, error as socket_error
+from datetime import datetime
+from pytz import reference
+
+__example_name__ = "pap_logger_example"
+
+
+def _get_timezone():
+    return reference.LocalTimezone().tzname(datetime.now())
 
 
 def _get_logging_dict(app_name):
+    tz = _get_timezone()
+    base_fmt = "%(module)s : %(message)s"
+    prefix_log_file_fmt = f'%(asctime)s.%(msecs)03d {tz} | [%(levelname)8s]'
+    base_ts = '%Y-%m-%d %H:%M:%S'
     return {
         'version': 1,
         'formatters': {
+            'logfile_with_host': {
+                'format': f'{prefix_log_file_fmt} | {gethostname()} | {base_fmt}',
+                'datefmt': base_ts
+            },
             'logfile': {
-                'format': '%(asctime)s [%(levelname)7s] | %(module)s : %(message)s',
-                'datefmt': '%Y-%m-%d %H:%M:%S %Z'
+                'format': f'{prefix_log_file_fmt} | {base_fmt}',
+                'datefmt': base_ts
             },
             'syslog': {
-                'format': f'%(asctime)s [%(levelname)7s] | {socket.gethostname()} | %(module)s : %(message)s',
-                'datefmt': '%Y-%m-%d %H:%M:%S %Z'
+                'format': f'[%(levelname)8s] | {base_fmt}',
+                'datefmt': base_ts
             },
             'simple': {
-                'format': '%(asctime)s %(message)s',
-                'datefmt': '%Y-%m-%d %H:%M:%S %Z'
+                'format': "%(asctime)s : %(message)s",
+                'datefmt': f'{base_ts} %Z'
             },
         },
         'handlers': {
@@ -37,40 +53,89 @@ def _get_logging_dict(app_name):
             app_name: {
                 'handlers': ['sysout'],
                 'propagate': True,
-                'level': 'DEBUG',
+                'level': 'WARNING',
             }
         },
     }
 
 
-class LsllLogger:
-    """A logger without a StreamHandler on sysout and optionally a SysLogHandler or a TimedRotatingFileHandler"""
+class PaPLogger:
+    """A logger with a StreamHandler on sysout and optionally a SysLogHandler and/or a TimedRotatingFileHandler"""
 
-    def __init__(self, app_name, verbose, debug):
+    def __init__(self, app_name, level=None, verbose_fmt=False, logfile_with_hostname=False, when='D', backup_count=15):
         self.logging_dict = _get_logging_dict(app_name)
-        self.verbose = verbose
-        self.debug = debug
-        self._log_file = None
-        self._syslog_host = None
-        self._syslog_port = None
-        self.level = logging.INFO if self.verbose else logging.WARNING
-
-        if self.debug:
-            self.level = logging.DEBUG
         logging.config.dictConfig(self.logging_dict)
         self.logger = logging.getLogger(app_name)
+        if level:
+            self.level = level
+        self._verbose_fmt = verbose_fmt
+        self._logfile_with_hostname = logfile_with_hostname
+        self._when = when
+        self._backup_count = backup_count
+        self._log_file = None
+        self._syslog_host = None
+        self._syslog_port = 514
+        self._level = logging.WARNING
+
         self._sysout_handler = self.logger.handlers[0]
         self._logfile_handler = None
         self._syslog_handler = None
 
         self._update_sysout_formatter()
+        self._update_logger_level()
         self._update_sysout_level()
 
+    def _update_logger_level(self):
+        self.logger.setLevel(self.level)
         level_name = logging.getLevelName(self.logger.getEffectiveLevel())
-        self.logger.info(f"Logging activated with level {level_name}")
+        self.logger.info(f"Logging with global level {level_name}")
 
-        level_name = logging.getLevelName(self._sysout_handler.level)
-        self.logger.info(f"Sysout logging activated with level {level_name}")
+    @property
+    def level(self) -> int:
+        """
+        Updates logging level at run time
+        """
+        return self._level
+
+    @level.setter
+    def level(self, value: int):
+        self._level = value
+        self._update_sysout_formatter()
+        self._update_logger_level()
+        self._update_sysout_level()
+        if self._syslog_handler:
+            self._update_syslog_handler()
+        if self._logfile_handler:
+            self._update_logfile_handler()
+
+    @property
+    def verbose_fmt(self) -> bool:
+        """
+        Updates verbosity at run time
+        """
+        return self._verbose_fmt
+
+    @verbose_fmt.setter
+    def verbose_fmt(self, value: bool):
+        self._verbose_fmt = value
+        self._update_sysout_formatter()
+
+    @property
+    def logfile_with_hostname(self) -> bool:
+        """
+        Adds the current hostname to logfile content and filename.
+        """
+        return self._logfile_with_hostname
+
+    @logfile_with_hostname.setter
+    def logfile_with_hostname(self, value: bool):
+        if value != self.logfile_with_hostname:
+            if not self.logfile_with_hostname and value:
+                self.log_file = self.log_file.parent / f"{gethostname()}_{self.log_file.name}"
+            elif self.logfile_with_hostname and not value:
+                self.log_file = self.log_file.parent / self.log_file.name.replace(f"{gethostname()}_", "")
+            self._logfile_with_hostname = value
+            self._update_logfile_formatter()
 
     @property
     def log_file(self) -> Path:
@@ -80,12 +145,30 @@ class LsllLogger:
         return self._log_file
 
     @log_file.setter
-    def log_file(self, value: Path):
-        self._log_file = Path(value)
-        if not self._logfile_handler:
-            self._add_logfile_handler()
+    def log_file(self, value: [Path, str]):
+        if value is None:
+            self._remove_logfile_handler()
+            self._log_file = value
+            return
+
+        if self.log_file:
+            self.logger.debug(f"Changing log file from {self.log_file} to {value}")
+
+        if isinstance(value, str):
+            self._log_file = Path(value)
         else:
-            self._update_logfile_handler()
+            self._log_file = value
+
+        try:
+            if not self._log_file.parent.exists():
+                self._log_file.parent.mkdir(parents=True)
+
+            if not self._logfile_handler:
+                self._add_logfile_handler()
+            else:
+                self._update_logfile_handler()
+        except PermissionError:
+            self.logger.error(f"Could not create {self.log_file.name} in {self.log_file.parent}: Permission Denied")
 
     @property
     def syslog_host(self) -> str:
@@ -96,11 +179,19 @@ class LsllLogger:
 
     @syslog_host.setter
     def syslog_host(self, value: str):
+        if value is None:
+            self._remove_syslog_handler()
+            self._syslog_host = value
+            return
+
         self._syslog_host = value
-        if not self._syslog_handler:
-            self._add_syslog_handler()
-        else:
-            self._update_syslog_handler()
+        try:
+            if not self._syslog_handler:
+                self._add_syslog_handler()
+            else:
+                self._update_syslog_handler()
+        except socket_error:
+            self.logger.error(f"Could not connect to syslog on {self.syslog_host}:{self.syslog_port}")
 
     @property
     def syslog_port(self) -> int:
@@ -126,12 +217,15 @@ class LsllLogger:
         return logging.Formatter(fmt=fmt_from_dict['format'], datefmt=fmt_from_dict['datefmt'])
 
     def _update_sysout_formatter(self):
-        sysout_fmt_name = 'logfile' if (self.verbose or self.debug) else 'simple'
+        sysout_fmt_name = 'logfile' if (self.verbose_fmt or self.level <= logging.WARNING) else 'simple'
         sysout_fmt = self._get_formatter_from_dict(sysout_fmt_name)
         self._sysout_handler.setFormatter(sysout_fmt)
+        self._sysout_handler.close()
 
     def _update_sysout_level(self):
         self._sysout_handler.level = self.level
+        level_name = logging.getLevelName(self._sysout_handler.level)
+        self.logger.info(f"Sysout logging with level {level_name}")
 
     def _add_syslog_handler(self):
         """Adds a SysLogHandler, with minimum logging set to WARNING and a 'verbose' format"""
@@ -140,19 +234,101 @@ class LsllLogger:
         self._syslog_handler.setFormatter(self._get_formatter_from_dict('syslog'))
         self.logger.addHandler(self._syslog_handler)
         level_name = logging.getLevelName(self._syslog_handler.level)
-        self.logger.debug(f"Added logging to syslog on {self.syslog_host}:{self.syslog_port} with level {level_name}")
+        self.logger.debug(f"Added SysLogHandler to {self.syslog_host}:{self.syslog_port} with level {level_name}.")
 
     def _update_syslog_handler(self):
-        self._syslog_handler.address = (self.syslog_host, self.syslog_port)
+        if self._syslog_handler:
+            self._syslog_handler.address = (self.syslog_host, self.syslog_port)
+
+    def _remove_syslog_handler(self):
+        if self._syslog_handler:
+            self.logger.debug("Removing SysLogHandler")
+            self.logger.removeHandler(self._syslog_handler)
 
     def _add_logfile_handler(self):
         """Adds a TimedRotatingFileHandler, with minimum logging set to DEBUG and a 'verbose' format"""
-        self._logfile_handler = TimedRotatingFileHandler(filename=self.log_file, when='D', backupCount=15)
+        try:
+            self._logfile_handler = TimedRotatingFileHandler(filename=self.log_file, when=self._when,
+                                                             backupCount=self._backup_count)
+        except ValueError as e:
+            self.logger.error(f"TimedRotatingFileHandler : {e}")
+            return
+
         self._logfile_handler.setLevel(logging.DEBUG)
-        self._logfile_handler.setFormatter(self._get_formatter_from_dict('logfile'))
+        self._update_logfile_formatter()
         self.logger.addHandler(self._logfile_handler)
         level_name = logging.getLevelName(self._logfile_handler.level)
-        self.logger.debug(f"Added logging to {self.log_file} with level  {level_name}")
+        self.logger.debug(f"Added TimedRotatingFileHandler to {self.log_file} with level {level_name}.")
+        self.logger.debug(f"Log rotates every {self._when} and keeps {self._backup_count} logs.")
+
+    def _update_logfile_formatter(self):
+        logfile_fmt_name = 'logfile_with_host' if self.logfile_with_hostname else 'logfile'
+        logfile_fmt = self._get_formatter_from_dict(logfile_fmt_name)
+        self._logfile_handler.setFormatter(logfile_fmt)
+        self._logfile_handler.close()
 
     def _update_logfile_handler(self):
-        self._logfile_handler.baseFilename = self.log_file
+        if self._logfile_handler:
+            self._logfile_handler.close()
+            self._logfile_handler.baseFilename = self.log_file
+
+    def _remove_logfile_handler(self):
+        if self._logfile_handler:
+            self.logger.debug("Removing TimedRotatingFileHandler")
+            self.logger.removeHandler(self._logfile_handler)
+
+
+def _pap_logger_example(verbose_fmt: bool, log_path: Path, syslog_host: str):
+    logging.raiseExceptions = False
+
+    lssl = PaPLogger(app_name="lssl_logger_example", verbose_fmt=verbose_fmt)
+    logger = lssl.get_logger()
+
+    if log_path and log_path.exists() and not log_path.is_dir():
+        logger.critical(f"{log_path} is not a directory")
+        return
+
+    syslog_host = "hostname_with_a_syslog_listening" if not syslog_host else syslog_host
+
+    def _log_in_all_levels(msg):
+        logger.debug(msg)
+        logger.info(msg)
+        logger.warning(msg)
+        logger.error(msg)
+        logger.critical(msg)
+
+    for level in (logging.DEBUG, logging.INFO, logging.WARNING, logging.ERROR, logging.CRITICAL):
+        lssl.level = level
+        lssl.log_file = log_path / f"{__example_name__}.log"
+        lssl.syslog_host = syslog_host
+        lssl.verbose_fmt = False
+        _log_in_all_levels("")
+        _log_in_all_levels(f"LEVEL SET TO {logging.getLevelName(level)} ({level})")
+        _log_in_all_levels(f"Hello from {__example_name__}")
+        lssl.logfile_with_hostname = True
+        _log_in_all_levels("with hostname")
+        lssl.logfile_with_hostname = False
+        _log_in_all_levels("without hostname")
+        lssl.syslog_host = "this_is_anunknown"
+        _log_in_all_levels("unknown Syslog")
+        lssl.syslog_host = None
+        _log_in_all_levels("remove Syslog")
+        lssl.log_file = None
+        _log_in_all_levels("remove log file")
+        if level >= logging.WARNING:
+            lssl.verbose_fmt = True
+            _log_in_all_levels("verbose")
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description=__example_name__,
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-v", "--verbose_fmt", help="Verbose formatting.", action="store_true")
+    parser.add_argument("-p", "--log_path", help="Path to log directory.", type=Path,
+                        default=Path("/tmp") / __example_name__)
+    parser.add_argument("-sh", "--syslog_host", help="Syslog hostname.", type=str)
+
+    args = parser.parse_args()
+    _pap_logger_example(verbose_fmt=args.verbose_fmt, syslog_host=args.syslog_host, log_path=args.log_path)
